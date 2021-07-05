4dac2a78c36dfdbcaee9ef74b913094627eeef51
Chintan Suthar <75067919+chintansuthar72@users.noreply.github.com>
2021-07-05 19:48:16 +0530

    Add files via upload

--- BSC_SOFT_DECISION DECODING/BSC_SoftDecisionDecoding.py
+++ BSC_SOFT_DECISION DECODING/BSC_SoftDecisionDecoding.py
@@ -3,34 +3,26 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import random
 
-mat = sio.loadmat(
-    'D:/CT Project/BSC_HARD_DECISION DECODING/H_matrix/Hmatrix.mat')  # Load Parity Check Matrix From Matlab File
+mat = sio.loadmat('H_matrix/Hmatrix.mat')  # Load Parity Check Matrix From Matlab File
 H = mat['H']  # Parity Check Matrix
 
-
 def BSC(encodedM, probability):
-    noisy = np.zeros(len(encodedM), dtype=int)  # noisy Message
-    count = 0
-    for i in encodedM:
+    n = len(encodedM)
+    noisy = np.zeros(n,dtype=int) # noisy Message
+    error = np.zeros(n,dtype=int)
+    for i in range(n):
         if random.random() <= probability:
-            if i == 0:
-                noisy[count] = 1
-            else:
-                noisy[count] = 0
-        else:
-            noisy[count] = i
-        count += 1
+            error[i] = 1
+    for i in range(n): noisy[i] = (encodedM[i]+error[i])%2
     return noisy
-
 
 dv = sum(np.transpose(H)[0])  # Degree of each VN
 dc = sum(H[0])  # Degree of each CN
 n = len(H[0])  # Length of Message
 u = len(H)  # Nos of Parity bits
 
-CNs = np.zeros((u, 2 * dc), dtype=int)  # for each CN - [connected VNs' index , that VNs' message]
-VNs = np.zeros((n, (2 * dv) + 1),
-               dtype=int)  # for each VN - [decoded Message bit , connected CNs' index , that CNs' message]
+CNs = np.zeros((u, 2 * dc))  # for each CN - [connected VNs' index , that VNs' message]
+VNs = np.zeros((n, (2 * dv) + 1))  # for each VN - [decoded Message bit , connected CNs' index , that CNs' message]
 
 # index storing for VNs
 for i in range(n):
@@ -69,7 +61,7 @@ for pError in p:
         # VN to CN for 0th iteration
         for i in range(u):
             for j in range(dc, 2 * dc):
-                CNs[i][j] = VNs[CNs[i][j - dc]][0]
+                CNs[i][j] = pError if (VNs[int(CNs[i][j - dc])][0]) == 0 else (1 - pError)
 
         # Loop Process
         for cycle in range(100):
@@ -78,63 +70,53 @@ for pError in p:
             for i in range(n):
                 codeword[i] = VNs[i][0]
 
-            # Check for all CNs if they satisfy even parity or not
-            # if yes then terminate loop process
-            count = 0
-            for c_n in range(u):
-                if sum(CNs[c_n][dc:2 * dc]) % 2 == 0:
-                    count += 1
-                else:
-                    break
-            if count == u:  # if yes then terminate loop process
-                break
-
             # CN to VN message passing (Sum modulo 2 process)
             for c_n in range(u):
-                sumOfAll_CNs = sum(CNs[c_n][dc:2 * dc])
+                prob=1
+                for idx in range(dc,2*dc):
+                    prob *= (1-2*CNs[c_n][idx])
                 for idx in range(dc):
-                    for x in range(1, 1 + dv):
-                        if VNs[CNs[c_n][idx]][x] == c_n:
-                            VNs[CNs[c_n][idx]][x + dv] = (sumOfAll_CNs - CNs[c_n][dc + idx]) % 2
+                    for k in range(1,dv+1):
+                        if VNs[int(CNs[c_n][idx])][k]==c_n and CNs[c_n][idx+dc]!=0.5:
+                            VNs[int(CNs[c_n][idx])][k+dv] = 0.5*(1-(prob/(1-2*CNs[c_n][idx+dc])))
                             break
 
             # Doing majority voting for each VN
             for i in range(n):
-                ones = sum(VNs[i][1 + dv:2 * dv + 1]) + ReceivedMessage[i]
-                zeros = dv + 1 - ones
-                if ones > zeros:
+                prob1 = pError if ReceivedMessage[i] == 0 else (1 - pError)
+                prob0 = 1 - prob1
+                for k in range(1 + dv,2 * dv + 1):
+                    prob1 *= VNs[i][k]
+                    prob0 *= (1-VNs[i][k])
+                if prob1 >= prob0:
                     VNs[i][0] = 1
-                elif zeros > ones:
+                else:
                     VNs[i][0] = 0
 
             # VN to CN message passing (Majority vote process)
-            for i in range(u):
-                for c_n in range(dc, 2 * dc):
-                    ones = ReceivedMessage[CNs[i][c_n - dc]] + sum(VNs[CNs[i][c_n - dc]][1 + dv:(2 * dv) + 1])
-                    zeros = dv + 1 - ones
-                    for v_n in range(1, dv + 1):
-                        if VNs[CNs[i][c_n - dc]][v_n] == i:
-                            temp_ones = ones
-                            temp_zeros = zeros
-                            if VNs[CNs[i][c_n - dc]][v_n + dv] == 1:
-                                temp_ones -= 1
-                            else:
-                                temp_zeros -= 1
-                            if temp_ones > temp_zeros:
-                                CNs[i][c_n] = 1
-                            elif temp_zeros > temp_ones:
-                                CNs[i][c_n] = 0
+            for i in range(n):
+                prob1 = pError if ReceivedMessage[i] == 0 else (1 - pError)
+                prob0 = 1 - prob1
+                for k in range(1 + dv,2 * dv + 1):
+                    prob1 *= VNs[i][k]
+                    prob0 *= (1-VNs[i][k])
+                for idx in range(1, dv + 1):
+                    p_1 = prob1/VNs[i][idx+dv] if VNs[i][idx+dv]!=0 else 1
+                    p_0 = prob0 /(1- VNs[i][idx + dv]) if (1- VNs[i][idx + dv])!=0 else 1
+                    for v_n in range(dc):
+                        if CNs[int(VNs[i][idx])][v_n]==i:
+                            CNs[int(VNs[i][idx])][v_n+dc] = p_1/(p_1+p_0)
                             break
 
             # check if previous iteration's decoded message and present iteration's decoded message is same or not
             # if yes then terminate loop process
-            check = True
-            for i in range(n):
-                if codeword[i] != VNs[i][0]:
-                    check = False
-                    break
-            if check:  # if yes then terminate loop process
-                break
+            # check = True
+            # for i in range(n):
+            #     if codeword[i] != VNs[i][0]:
+            #         check = False
+            #         break
+            # if check:  # if yes then terminate loop process
+            #     break
 
         # check for if we got real transmitted message or not
         message_check = True
@@ -155,6 +137,6 @@ plt.xticks(p)
 plt.xlim([0, 1])
 plt.ylim([0, 1])
 plt.ylabel('Success Probability of Decoding')
-plt.title('BSC Hard Decision Decoding')
+plt.title('BSC Soft Decision Decoding')
 plt.grid()
 plt.show()
